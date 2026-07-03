37305043e9c93324dbf80b6b37347e0dc2efe012
Kashif <outtanowhere78@gmail.com>
2026-07-03 07:31:28 +0530

    Updated Repos!

--- src/main/java/com/Kashif/blog_api/repository/CommentRepository.java
+++ src/main/java/com/Kashif/blog_api/repository/CommentRepository.java
@@ -1,4 +1,14 @@
 package com.Kashif.blog_api.repository;
 
-public interface CommentRepository {
+import com.Kashif.blog_api.entity.Comment;
+import com.Kashif.blog_api.entity.Post;
+import com.Kashif.blog_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+    List<Comment> findbyPost(Post post);
+    List<Comment> findbyUser(User user);
 }
--- src/main/java/com/Kashif/blog_api/repository/PostRepository.java
+++ src/main/java/com/Kashif/blog_api/repository/PostRepository.java
@@ -1,4 +1,13 @@
 package com.Kashif.blog_api.repository;
 
-public interface PostRepository {
+import com.Kashif.blog_api.entity.Post;
+import com.Kashif.blog_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findByAuthor(User author);
+    List<Post> findAllByOrderByCreatedAtDesc();
 }
