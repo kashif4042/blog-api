99bc86c128a7042c66d7af9ae2220294d7ee1aad
Kashif <outtanowhere78@gmail.com>
2026-07-05 05:10:08 +0530

    CommentService Interface

--- src/main/java/com/Kashif/blog_api/service/CommentService.java
+++ src/main/java/com/Kashif/blog_api/service/CommentService.java
@@ -1,4 +1,19 @@
 package com.Kashif.blog_api.service;
 
+import com.Kashif.blog_api.entity.Comment;
+import com.Kashif.blog_api.entity.Post;
+import com.Kashif.blog_api.entity.User;
+
+import java.util.List;
+
 public interface CommentService {
+    Comment createComment(Long postId,Long userId, Comment comment);
+
+    Comment getCommentById(Long id);
+
+    List<Comment> getCommentsByPost(Post post);
+
+    List<Comment> getCommentsByUser(User user);
+
+    void deleteComment(Long id);
 }
