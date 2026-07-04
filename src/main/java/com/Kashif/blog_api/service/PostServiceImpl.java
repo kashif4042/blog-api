a5187f9b09d63e2e1332a00d9ca1a85030a22911
Kashif <outtanowhere78@gmail.com>
2026-07-05 04:58:13 +0530

    Updated PostService Implementation!

--- src/main/java/com/Kashif/blog_api/service/PostServiceImpl.java
+++ src/main/java/com/Kashif/blog_api/service/PostServiceImpl.java
@@ -1,4 +1,67 @@
 package com.Kashif.blog_api.service;
 
-public class PostServiceImpl {
+
+import com.Kashif.blog_api.entity.Post;
+import com.Kashif.blog_api.entity.User;
+import com.Kashif.blog_api.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Override
+    public Post createPost(Post post) {
+        post.setCreatedAt(LocalDateTime.now());
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post getPostById(Long id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Post not found with id:" + id));
+
+
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+
+    }
+
+    @Override
+    public List<Post> getPostsByAuthor(User author) {
+        return postRepository.findByAuthor(author);
+
+    }
+
+    @Override
+    public Post updatePost(Long id, Post updatedPost) {
+        Post existingPost = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Post not found with id:" + id));
+
+        existingPost.setTitle(updatedPost.getTitle());
+        existingPost.setContent(updatedPost.getContent());
+        return postRepository.save(existingPost);
+
+    }
+    public void deletePost(Long id){
+        if (!postRepository.existsById(id)) {
+            throw new RuntimeException("Post not found with id: " + id);
+        }
+        postRepository.deleteById(id);
+    }
+
 }
+
+
+
+
+
