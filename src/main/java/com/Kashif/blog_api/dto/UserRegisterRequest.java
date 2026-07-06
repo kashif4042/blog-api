5ef3aaa11ca77cb7217c3eb86366d02886e078c3
Kashif <outtanowhere78@gmail.com>
2026-07-06 13:04:37 +0530

    dto layer

--- src/main/java/com/Kashif/blog_api/dto/UserRegisterRequest.java
+++ src/main/java/com/Kashif/blog_api/dto/UserRegisterRequest.java
@@ -1,4 +1,21 @@
 package com.Kashif.blog_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
--- src/main/java/com/Kashif/blog_api/dto/UserResponse.java
+++ src/main/java/com/Kashif/blog_api/dto/UserResponse.java
@@ -1,4 +1,17 @@
 package com.Kashif.blog_api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
 }
