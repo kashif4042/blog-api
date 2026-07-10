af3d12d317f32e839d35be10127076db0900b226
Kashif <outtanowhere78@gmail.com>
2026-07-10 23:57:19 +0530

    Comment DTOs updated!

--- src/main/java/com/Kashif/blog_api/dto/CommentRequest.java
+++ src/main/java/com/Kashif/blog_api/dto/CommentRequest.java
@@ -1,4 +1,13 @@
 package com.Kashif.blog_api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CommentRequest {
+
+    @NotBlank(message = "Content is required")
+    private String content;
 }
--- src/main/java/com/Kashif/blog_api/dto/CommentResponse.java
+++ src/main/java/com/Kashif/blog_api/dto/CommentResponse.java
@@ -1,4 +1,20 @@
 package com.Kashif.blog_api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentResponse {
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    private Long postId;
+    private UserResponse user;
 }
