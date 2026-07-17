8100ce1210be56b9ea8e7258e72e5c9b2926da3f
Kashif <outtanowhere78@gmail.com>
2026-07-18 03:06:17 +0530

    Connect Spring Security to database via UserDetailsService and AuthenticationProvider

--- src/main/java/com/Kashif/blog_api/config/SecurityConfig.java
+++ src/main/java/com/Kashif/blog_api/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.Kashif.blog_api.config;
 
+import com.Kashif.blog_api.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
    @Bean
    public PasswordEncoder passwordEncoder(){
@@ -25,4 +32,20 @@ public class SecurityConfig {
                );
        return http.build();
     }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+       DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+       provider.setUserDetailsService(userDetailsService);
+       provider.setPasswordEncoder(passwordEncoder());
+       return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception{
+       return config.getAuthenticationManager();
+    }
+
+
 }
---
+++ src/main/java/com/Kashif/blog_api/security/CustomUserDetails.java
@@ -0,0 +1,50 @@
+package com.Kashif.blog_api.security;
+
+import com.Kashif.blog_api.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private User user;
+
+    public CustomUserDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return user.getUsername();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+
+    public User getUser() {
+        return user;
+    }
+
+}
