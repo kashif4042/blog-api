6d893a2467500338c9446d57ae0035cdf8e5a35a
Kashif <outtanowhere78@gmail.com>
2026-07-21 20:38:40 +0530

    Add JwtAuthFilter

--- src/main/java/com/Kashif/blog_api/config/SecurityConfig.java
+++ src/main/java/com/Kashif/blog_api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.Kashif.blog_api.config;
 
 import com.Kashif.blog_api.security.CustomUserDetailsService;
+import com.Kashif.blog_api.security.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,44 +9,50 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-   @Bean
-   public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http.csrf(csrf-> csrf.disable())
-               .authorizeHttpRequests(auth-> auth
-                       .requestMatchers("/api/users/register", "/api/users/login").permitAll()
-                       .anyRequest().authenticated()
-               );
-       return http.build();
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-       DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-       provider.setUserDetailsService(userDetailsService);
-       provider.setPasswordEncoder(passwordEncoder());
-       return provider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
-            throws Exception{
-       return config.getAuthenticationManager();
-    }
-
-
 }
